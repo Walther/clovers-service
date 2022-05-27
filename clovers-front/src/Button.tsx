50f27f85a98232e54ff64cfe1ba899d686287069
Walther <veeti.haapsamo@gmail.com>
2022-05-27 03:22:12 +0300

    Frontend: add initial DeleteButton, even if half-functioning

--- clovers-front/src/Button.tsx
+++ clovers-front/src/Button.tsx
@@ -4,7 +4,13 @@ import React from "react";
 export const Button = ({
   handleClick,
   text,
+  className,
 }: {
   handleClick: React.MouseEventHandler;
-  text: String;
-}) => <button onClick={handleClick}>{text}</button>;
+  text: string;
+  className?: string;
+}) => (
+  <button className={className ? className : "Button"} onClick={handleClick}>
+    {text}
+  </button>
+);
--- clovers-front/src/Objects/Quad.tsx
+++ clovers-front/src/Objects/Quad.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import { DeleteButton } from "../DeleteButton";
 import { TextInput, TripleNumberInput } from "../Input";
 import { Material, MaterialForm } from "../Materials/Material";
 
@@ -19,9 +20,12 @@ export const QuadForm = ({
   path: any; // TODO: ramda path type
   setState: Function;
 }): ReactElement => {
+  // removing the title from the path for deleting; TODO: remove when adding `kind` to objects and reducing nesting in upstream
+  const deletePath = path.slice(0, -1);
   return (
     <div className="OptionsForm">
       <h3>quad</h3>
+      <DeleteButton object={object} path={deletePath} setState={setState} />
       <TextInput
         fieldname="comment"
         object={object}
